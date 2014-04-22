9441a0a0886da60eefe084626238131d2771dbdd
unknown <mohamed_r@SDW1965.spanservices.com>
2014-04-22 12:39:33 +0530

    Added

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
-$dbhost = 'localhost';
-$dbuser = 'root';//user of the database
-$dbpass = '';//password here
+$dbhost = '127.4.182.2:3306';
+$dbuser = 'adminQMjwDl7';//user of the database
+$dbpass = 'qbgK6Ym6ZjF2';//password here
 
 $conn = mysql_connect($dbhost, $dbuser, $dbpass) or die ('Error connecting to mysql');//Connect to MySQL
 
-$db = 'abhatrav_cloudbill_stock';//database name
+$db = 'rkinvoice';//database name
 mysql_select_db($db); // choose databse
 ?>
